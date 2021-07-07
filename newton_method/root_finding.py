42efd0faa908513e8e618c8c8e81fd1178a1a350
enfow <wgm0601@gmail.com>
2021-07-07 09:50:20 +0900

    Add result

--- newton_method/root_finding.py
+++ newton_method/root_finding.py
@@ -1,3 +1,4 @@
+"""Find root of 2 with Newton's Method."""
 import math
 
 def F(x: float) -> float:
@@ -13,3 +14,11 @@ if __name__ == "__main__":
     for k in range(ITER):
         print(f"iter: {k} | x: {round(x, 6): 9} | x - x*: {x - x_star}")
         x = x - (1 / diff_F(x)) * F(x)
+
+"""
+iter: 0 | x:       1.0 | x - x*: -0.41421356237309515
+iter: 1 | x:       1.5 | x - x*: 0.08578643762690485
+iter: 2 | x:  1.416667 | x - x*: 0.002453104293571595
+iter: 3 | x:  1.414216 | x - x*: 2.1239014147411694e-06
+iter: 4 | x:  1.414214 | x - x*: 1.5947243525715749e-12
+"""
